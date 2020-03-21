a819c1f6718b8b57678b3310dee204ef1f99b617
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2020-03-21 23:10:15 +0100

    Modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@
 
 Rails.application.routes.draw do
   constraints IpAddressMemberConstraint.new do
-    defaults format: false do
-      get '/*path', to: 'ip_addresses#show'
-      delete '/*path', to: 'ip_addresses#destroy'
-      put '/*path', to: 'ip_addresses#create'
-    end
+    get '/*path', to: 'ip_addresses#show'
+    delete '/*path', to: 'ip_addresses#destroy'
+    put '/*path', to: 'ip_addresses#create'
   end
 end
